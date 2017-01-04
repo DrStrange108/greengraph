89bb830c30b81714d241ae5c649941b5b0ba4b03
kunalvora1 <kunal.vora1@gmail.com>
2017-01-04 17:31:46 +0000

    Added Argparse parameters

--- green.py
+++ green.py
@@ -3,9 +3,24 @@
 from matplotlib import pyplot as plt
 from greengraph import Greengraph
 
+from argparse import ArgumentParser
+if __name__ == "__main__":
+    parser = ArgumentParser(description = "Generate Graph of Green Steps")
+    parser.add_argument('--from', '-f')
+    parser.add_argument('--to','-t')
+    parser.add_argument('--steps', '-s')
+    parser.add_argument('--out', '-o')
+    arguments= parser.parse_args()
 
-mygraph=Greengraph('New York','Chicago')
-data = mygraph.green_between(20)
+
+
+mygraph=Greengraph(arguments.from,arguments.to)
+data = mygraph.green_between(arguments.steps)
 
 plt.plot(data)
 plt.show()
+
+
+
+
+
