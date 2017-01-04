989654dea0d19f2922eefe260f6b9f5aa8f018f0
kunalvora1 <kunal.vora1@gmail.com>
2017-01-04 17:00:27 +0000

    Added script to work from command line

---
+++ green.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from matplotlib import pyplot as plt
+from greengraph import Greengraph
+
+
+mygraph=Greengraph('New York','Chicago')
+data = mygraph.green_between(20)
+
+plt.plot(data)
+plt.show()
