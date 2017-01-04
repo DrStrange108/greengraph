4512b15af78afad36a0252c55b71004568220c18
kunalvora1 <kunal.vora1@gmail.com>
2017-01-04 18:49:03 +0000

    Fully Functions at Command Line

--- green.py
+++ green.py
@@ -6,19 +6,25 @@ from greengraph import Greengraph
 from argparse import ArgumentParser
 if __name__ == "__main__":
     parser = ArgumentParser(description = "Generate Graph of Green Steps")
-    parser.add_argument('--from', '-f')
-    parser.add_argument('--to','-t')
+    parser.add_argument('--origin', '-o')
+    parser.add_argument('--destination','-d')
     parser.add_argument('--steps', '-s')
-    parser.add_argument('--out', '-o')
+    parser.add_argument('--filename', '-f')
     arguments= parser.parse_args()
 
 
+    greeting = arguments.origin + " " + arguments.destination + " " + arguments.steps+ " " + arguments.filename
+ 
+    print greeting
 
-mygraph=Greengraph(arguments.from,arguments.to)
+mygraph=Greengraph(arguments.origin,arguments.destination)
 data = mygraph.green_between(arguments.steps)
 
 plt.plot(data)
 plt.show()
+
+# Got it to work after removing arguments.from (turns out it's a keyword in python)
+# Need to add argparse fix for names with spaces to work (eg: New York)
 
 
 
